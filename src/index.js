315d88df4cdec5b4a08aff4e020bb5f73b9d1dad
shenhailong <shenhailong@rocoinfo.com>
2018-11-02 15:08:47 +0800

    提交一版

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios'
+axios.get('/api/wap/coupon/center').then((res) => {
+  console.log(4)
+})
+
+axios.get(`/free/member/sendForgetCode?mobilePhone=18600206984&openId=%20abcddkdkd&_=1541126434373`).then( res => {
+  console.log(5)
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
